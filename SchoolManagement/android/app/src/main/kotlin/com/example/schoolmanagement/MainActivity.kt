c52a1d96c9d70076ab522bf63970c967de23b5d0
Mohammed Abdullkareem <sonsabdulkareem@gmail.com>
2025-06-16 15:33:06 +0300

    Delete SchoolManagement directory

--- SchoolManagement/android/app/src/main/kotlin/com/example/schoolmanagement/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.schoolmanagement
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- SchoolManagement/lib/core/class/subject.dart
+++
@@ -1,6 +0,0 @@
-class Subject {
-  final int id;
-  final String subjectName;
-  final String teacherName;
-  Subject(this.id, this.subjectName, this.teacherName);
-}
