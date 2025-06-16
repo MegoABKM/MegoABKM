5d639fc0036e0c13fbc6bd703e0e768d4c2f522c
Mohammed Abdullkareem <sonsabdulkareem@gmail.com>
2025-06-16 15:32:41 +0300

    Delete CompanyTaskMaster directory

--- CompanyTaskMaster/Backend/testnotification.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "functions.php";
-
-echo "🔹 Sending test notification...\n";
-$response = sendNotificationToTopicwithcomposer("Topic", "Test message", "news");
-echo "🔹 Function Output: " . json_encode($response) . "\n";
-echo "✅ Done.\n";
--- CompanyTaskMaster/Frontend/android/app/src/main/kotlin/com/example/tasknotatebeta/MainActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tasknotatebeta
-
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity()
